67229a5b63d371bf5df73586d8e513ba2a959d19
Noah Hradek <nhradek@alexinc.com>
2021-12-04 10:05:52 -0700

    modified dive for aim

--- day2/dive.hs
+++ day2/dive.hs
@@ -1,14 +1,14 @@
 import           System.IO
 
 
-move :: (Int, Int) -> (String, Int) -> (Int, Int)
-move (x, y) pair = case pair of
-    ("forward", n) -> (x + n, y)
-    ("up"     , n) -> (x, y - n)
-    ("down"   , n) -> (x, y + n)
-    (_        , n) -> (x, y)
+move :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
+move (x, y, aim) pair = case pair of
+    ("forward", n) -> (x + n, y + aim * n, aim)
+    ("up"     , n) -> (x, y, aim - n)
+    ("down"   , n) -> (x, y, aim + n)
+    (_        , n) -> (x, y, aim)
 
-driveSubmarine :: [(String, Int)] -> (Int, Int) -> (Int, Int)
+driveSubmarine :: [(String, Int)] -> (Int, Int, Int) -> (Int, Int, Int)
 driveSubmarine commands initPos = foldl move initPos commands
 
 main :: IO ()
@@ -18,7 +18,11 @@ main = do
         let input    = map words $ lines contents
         -- convert list to tuple of string and integer
         let commands = [ (x !! 0, read (x !! 1) :: Int) | x <- input ]
-        let finalPos = driveSubmarine commands (0, 0)
+        let finalPos = driveSubmarine commands (0, 0, 0)
         print finalPos
         -- get second item of finalPOs
-        let prod = (fst finalPos) * (snd finalPos) in print prod
+        -- get first and second item of finalPos
+
+        let 
+            (x, y, _) = finalPos
+            prod = x * y in print prod
