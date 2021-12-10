50ceba1da2e05028cf3838e424bb3c3fa185c6c0
Noah Hradek <nhradek@alexinc.com>
2021-12-10 10:41:01 -0700

    day 7 part 2 finished

--- day7/whales.py
+++ day7/whales.py
@@ -1,18 +1,22 @@
 
-
-def arith_sum(n):
-    return n * (n+1) / 2
-
 def calc_fuel_cost(crabs):
     max_crab = max(crabs)
     min_pos = 0
     min_cost = 0
+    costs = {} # dictionary to store costs
     for crab in crabs:
-        min_cost += arith_sum(crab)
+        min_cost += sum(i for i in range(1, crab+1))
 
     for pos in range(1, max_crab):
-        cost = sum(arith_sum(crab - pos) for crab in crabs)
-        print(cost)
+        cost = 0
+        for crab in crabs:
+            s = 0
+            if (crab, pos) not in costs or (pos, crab) not in costs:
+                s = sum(i for i in range(1, abs(crab - pos) + 1))
+                costs[(crab, pos)] = s
+            else:
+                s = costs[(crab, pos)] if (crab, pos) in costs else costs[(pos, crab)]
+            cost += s
         if cost < min_cost:
             min_cost = cost
             min_pos = pos
